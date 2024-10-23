d3fc60fffebcbbbe5da4e711c50baa4d8950be13
Matheus Fonseca <61693870+Math2034@users.noreply.github.com>
2024-10-23 10:51:40 +0800

    Add files via upload

---
+++ js/handling.js
@@ -0,0 +1,35 @@
+// JavaScript Assessment 1: Task 2 - Event Handling
+
+//No need for globals here
+
+
+
+function addBlueBox() {
+
+    //Last button function here    
+    var bluebox = document.getElementById('blueIn');
+    bluebox.style.display = 'block'; 
+
+}
+
+function addBlueBottom() {
+
+    //Last button function here    
+    var blueboxBottom = document.getElementById('blueBottom');
+    blueboxBottom.style.display = 'block'; 
+
+}
+
+// function() {//Fisrt button function here alert("Don't forget to use strict in Function Name here");}
+
+function init(){
+alert("Don't forget to use strict in init function");
+
+
+// All event listeners here
+
+
+}
+
+
+window.onload = init;
